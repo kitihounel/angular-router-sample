1db2e73ef86da94a983c8050e41eb95a724287fe
Kitihounel <kitihounel@gmail.com>
2020-12-03 20:51:40 +0100

    Integrated changes of milestone 3

---
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero;
+
+  constructor(private route: ActivatedRoute, private router: Router, private service: HeroService) { }
+
+  ngOnInit() {
+  }
+}
